93ff2e6398265d921090233b70f20652501ca9ae
Matthew Thomas <matthewthomas@Matthews-MacBook-Pro.local>
2019-08-21 23:28:05 -0700

    can now trim video with datapoints given by user of web application

--- djangonautic/djangonautic/views.py
+++ djangonautic/djangonautic/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .videotrim import videotrim
+
+video_file_path = '/Users/matthewthomas/Downloads/IMG_4135.MOV'
+
 def home(request):
     #return HttpResponse('home')
     return render(request, 'index.html')
 
 def trim(request):
     #if request.method == 'POST':
-    print(request.POST.getlist('mylist'))
+    #print(request.POST.getlist('mylist'))
+    videotrim(video_file_path, request.POST.getlist('mylist'))
     return render(request, 'index.html')
